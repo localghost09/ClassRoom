2b0875fefc9dc5bf0978ac86e74ab9ebb85fa582
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-04-24 23:46:03 +0530

    init

--- JS/JavaScript 7/app.js
+++ JS/JavaScript 7/app.js
@@ -25,3 +25,77 @@
 // implicit return
 
 const mul = (a,b) => a*b;
+
+
+// Set Timeout
+
+// console.log("he there");
+
+// // setTimeout(() =>{
+// //     console.log('apna college');
+    
+// // },400);
+
+// console.log('welcome to ');
+
+// // Set Intervals
+
+// const id = setInterval(()=>{
+//     console.log('tech nikhil');
+    
+// },2000)
+
+// console.log(id);
+// clearInterval(id);
+
+
+// const square=(n) => n*n;
+
+// const id = setInterval(()=>{
+//     console.log("hello nikhil");
+    
+// },2000)
+
+// setTimeout(()=>{
+//     clearInterval(id);
+// },10000)
+
+
+
+//WriteanarrowfunctionnamedarrayAveragethatacceptsanarrayofnumbersandreturnstheaverageofthosenumbers.
+// const arr = [1,2,3,4,5];
+
+// const length = arr.length;
+// console.log(length);
+
+
+// const arrayAverage = (arr) =>{
+//     let total = 0;
+//     for(let numbers of arr){
+//         total +=numbers;
+//     } 
+
+//     return total/length
+// }
+
+// console.log(arrayAverage(arr));
+
+
+
+//Qs2.WriteanarrowfunctionnamedisEven()thattakesasinglenumberasargumentandreturnsifitisevenornot
+// const isEven = (n) =>{
+//     return n%2==0?"even":"odd";
+// }
+
+//Whatistheoutputofthefollowingcode
+
+const objects = {
+    message :'hellow , world!',
+
+    logMessage() {
+        console.log(this.message);
+        
+    }
+};
+
+setTimeout(objects.logMessage,1000);
