38e70b3881abcde13aa1a892c44276ab726e8fb1
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-04-24 22:31:04 +0530

    init

---
+++ JS/JavaScript 7/app.js
@@ -0,0 +1,23 @@
+// try & catch
+// the try statements allows you to define a block of code to be tested for errors while it is being executed
+
+// the catch allows you to define a block of code to be executed , if an error occurs in the try block 
+
+
+// try{
+//     console.log(a);
+    
+// }catch(error){
+//     console.log('caught an error a is not defined');
+//     console.log(error);
+    
+// }
+
+
+// Arrow Functions
+
+
+// const sum = (a,b) =>{
+//     console.log(a+b);
+    
+// }
