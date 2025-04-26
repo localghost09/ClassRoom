a4516d52ad0461a462bb0cf2f107dfbdb8f2a6e2
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-04-26 22:41:07 +0530

    filter updates

--- JS/JavaScript 8/app.js
+++ JS/JavaScript 8/app.js
@@ -1,8 +1,43 @@
 // forEach 
 
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
-arr.forEach((el)=>{
-    console.log(el);
+// arr.forEach((el)=>{
+//     console.log(el);
     
+// });
+
+// let arr = [{
+//     name:'nikhil',
+//     age:20,
+// },
+// {
+//     name:'aditi',
+//     age:18,
+// },
+// {
+//     name:'kannu',
+//     age:24,
+// }];
+
+// arr.forEach((students =>{
+//     console.log(students.age);
+    
+// }));
+
+
+//Map
+
+// let num = [1,2,3,4];
+
+// let double = num.map((el)=>{
+//     return el * 2;
+// })
+
+//Filter 
+
+let nums = [1,2,3,4,7,8,2,9,10,12,11];
+
+let ans = nums.filter((el)=>{
+    return el % 2 ==0;
 });
