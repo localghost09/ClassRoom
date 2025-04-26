f74a87a808350e9d7f08c291d0f4f2d4d8d9429e
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-04-26 22:42:23 +0530

    init

--- JS/JavaScript 8/app.js
+++ JS/JavaScript 8/app.js
@@ -36,8 +36,8 @@
 
 //Filter 
 
-let nums = [1,2,3,4,7,8,2,9,10,12,11];
+// let nums = [1,2,3,4,7,8,2,9,10,12,11];
 
-let ans = nums.filter((el)=>{
-    return el % 2 ==0;
-});
+// let ans = nums.filter((el)=>{
+//     return el % 2 ==0;
+// });
