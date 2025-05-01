fc6d260a4a98964075e5abd770dd18905536c5f4
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-01 23:08:28 +0530

    getElementByClassName

--- JS DOM/app.js
+++ JS DOM/app.js
@@ -7,3 +7,12 @@
 // console.log(obj);
 
 // console.dir(obj);
+
+
+//getElementByClassName
+//returns the elements as an HTML collection or empty collection (if not found)
+
+
+// console.log(document.getElementsByClassName("oldImg"));
+// // give the value of first image 
+// console.log(document.getElementsByClassName("oldImg")[0]);
