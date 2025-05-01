08b258fe6e3470f17876dafd77bcd7d7f40ba85a
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-01 22:58:12 +0530

    getElementById

--- JS DOM/app.js
+++ JS DOM/app.js
@@ -2,3 +2,8 @@
 
 // getElementById
 // returns the element as an object or null(if not found)
+// document.getElementById("description");
+// const obj = document.getElementById("description");
+// console.log(obj);
+
+// console.dir(obj);
