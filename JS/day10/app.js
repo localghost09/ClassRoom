1d6ef7e6587b3bd99de7e4fc91f972e1168a5ff8
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-04 20:43:29 +0530

    Event Listener

--- JS/day10/app.js
+++ JS/day10/app.js
@@ -1,9 +1,34 @@
 //DOM Events 
 //events are signals that something has occured. (user inputs/actions)
 
-let btn = document.querySelector("button");
-console.dir(btn);
-btn.onclick = function(){
-    console.log('button was clicked');
+let btns = document.querySelectorAll("button");
+
+// for(btn of btns){
+//     btn.onclick = function(){
+//         console.log('you clicked a button');
+        
+//     }
+//     btn.onmouseenter = function(){
+//         console.log('you enterd a button');
+        
+//     }
+// }
+
+
+//Event Listener
+//JavaScript's event listener function allows you to create custom responses to events like mouse clicks, keyboard clicks, and window resizing
+
+// for(btn of btns){
+//     btn.addEventListener('click',sayhello);
+//     btn.addEventListener('click',sayname);
+// }
+
+// function sayhello(){
+//     alert('hello');
     
-}
+// }
+
+// function sayname(){
+//     alert('nikhil');
+    
+// }
