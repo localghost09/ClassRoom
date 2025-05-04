22a96c7813acecbf06dee1ba7c0963a0fb7ef777
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-04 21:06:23 +0530

    Activity Page

--- JS/day10/app.js
+++ JS/day10/app.js
@@ -1,7 +1,7 @@
 //DOM Events 
 //events are signals that something has occured. (user inputs/actions)
 
-let btns = document.querySelectorAll("button");
+// let btns = document.querySelectorAll("button");
 
 // for(btn of btns){
 //     btn.onclick = function(){
@@ -31,4 +31,30 @@ let btns = document.querySelectorAll("button");
 // function sayname(){
 //     alert('nikhil');
     
+// }
+
+
+
+//Activity 
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click",function(){
+//     let h3 = document.querySelector('h3');
+//     let randomColor = getRandom();
+//     h3.innerText=randomColor;
+
+//     let div = document.querySelector('div');
+//     div.style.backgroundColor=randomColor;
+
+    
+// })
+
+// function getRandom(){
+//     let red = Math.floor(Math.random()*255);
+//     let green = Math.floor(Math.random()*255);
+//     let blue = Math.floor(Math.random()*255);
+
+//     let color = `rgb(${red},${green},${blue})`;
+//     return color;
 // }
