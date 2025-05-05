f0f225809799760552796a68ebef3d045a288b3d
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-05 20:34:34 +0530

    init

--- JS/day10/app.js
+++ JS/day10/app.js
@@ -68,3 +68,29 @@
 // pr.addEventListener("click", function(){
 //     alert('try to copy ');
 // })
+
+
+
+// this is Event Listeners 
+// when this is used in a callback of event handler of something, it refers to that something
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "blue";
+// })
+
+// let p = document.querySelector("p");
+// let h1 = document.querySelector("h1");
+// let h3 = document.querySelector("h3");
+
+// function changeColor(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "blue";
+// }
+
+// btn.addEventListener("click", changeColor);
+// p.addEventListener("click", changeColor);
+// h1.addEventListener("click",changeColor);
+// h3.addEventListener("click", changeColor);
