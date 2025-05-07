f7159184cf3c98e35862f0df8c51d064c2704e66
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-07 23:25:15 +0530

    Extracting Form Data 2

--- JS/day10/app.js
+++ JS/day10/app.js
@@ -151,3 +151,19 @@
 // })
 
 
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let user = document.querySelector("#user");
+    let pass = document.querySelector("#pass");
+
+    console.log(user.value);
+    console.log(pass.value);
+    
+
+    alert(`Hi ${user.value}, your password is set to ${pass.value}`)
+    
+});
+
