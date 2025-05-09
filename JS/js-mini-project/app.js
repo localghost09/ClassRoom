33cbeafba998cbfb95d8ed2bbeeee252b878d4f7
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-09 23:37:04 +0530

    init

--- JS/js-mini-project/app.js
+++ JS/js-mini-project/app.js
@@ -35,9 +35,36 @@
 // btn.addEventListener("click", function(){
 //     let item = document.createElement("li");
 //     item.innerText = input.value;
+//     let delBtns = document.createElement("button");
+//     delBtns.innerText = "Delete";
+//     delBtns.classList.add("delete");
+//     item.appendChild(delBtns);
 //     ul.appendChild(item); // add the item value in ul as a child
     
 //     input.value="";
     
 // })
+
+
+// ul.addEventListener("click", function(event){
+//    if(event.target.nodeName=="BUTTON"){
+//     let listItem = event.target.parentElement;
+//     listItem.remove();
+//     console.log(delte);
+    
+//    }
+
+    
+// })
+
+// let debtns = document.querySelectorAll(".delete");
+
+// for(debtns of debtns){
+//     debtns.addEventListener("click", function(){
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+        
+//     });
+// }
 
