f65a516bb863af7dc99b2c09323332c5f909135e
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-09 22:57:50 +0530

    event bubling

---
+++ JS/js-mini-project/app.js
@@ -0,0 +1,25 @@
+// event bubling
+
+// let div = document.querySelector("div");
+// let ul  = document.querySelector("ul");
+// let li = document.querySelectorAll("li");
+
+
+// div.addEventListener("click", function(){
+//     console.log("div was clicked");
+    
+// })
+
+// ul.addEventListener("click", function(e){
+//     e.stopPropagation(); // to avoid event bubling
+//     console.log("ul was clicked");
+    
+// })
+
+// for(li of li){
+//     li.addEventListener("click", function(e){
+//          e.stopPropagation(); // to avoid event bubling
+//         console.log("li was clicked");
+        
+//     })
+// }
