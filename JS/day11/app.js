8e7e38aeae895424b7a3c36a6819d38de9c366af
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-10 20:52:52 +0530

    callback hell

--- JS/day11/app.js
+++ JS/day11/app.js
@@ -14,3 +14,39 @@ function three(){
     console.log(ans);
     
 }
+
+//three();
+
+// using breakpoint in source we can see how a function call another function and also we can see the call stack
+
+
+//Js is single threaded
+
+// setTimeout(()=>{
+//     console.log("hey");
+    
+// },2000);
+
+// console.log("happy birthday adi");
+
+// h1 = document.querySelector("h1");
+
+// function changeColor(color,delay,nextColorChange){
+//     setTimeout(()=>{
+//         h1.style.color= color;
+//         if(nextColorChange) nextColorChange();
+//     },delay);
+// }
+
+
+// changeColor("red",1000,()=>{
+//     changeColor("orange", 1000,()=>{
+//         changeColor("green", 1000, ()=>{
+//             changeColor("yellow", 1000, ()=>{
+//                 changeColor("blue", 1000);
+//             })
+//         })
+//     })
+// })
+
+// callback nesting -> callback hell
