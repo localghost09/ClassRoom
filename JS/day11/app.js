5511f32ada8f88cb169909bb8795b65fdf89ef75
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-10 20:26:15 +0530

    visualization of call stack

---
+++ JS/day11/app.js
@@ -0,0 +1,16 @@
+// visualization of call stack
+
+function one(){
+    return 1;
+}
+
+function two(){
+    return one() + one();
+
+}
+
+function three(){
+    let ans = two() + one();
+    console.log(ans);
+    
+}
