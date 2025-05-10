e9be2b616d3af4ae2fd4ae057a2600b7ea0ac982
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-10 20:13:30 +0530

    init

--- JS/js-mini-project/app.js
+++ JS/js-mini-project/app.js
@@ -68,3 +68,56 @@
 //     });
 // }
 
+
+// Simon Says Game
+
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red", "yellow", "green", "purple"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function(){
+    if(started==false){
+        console.log("game is started");
+        started = true;
+        
+        levelUp();
+    }
+})
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash")
+    },100);
+   
+}
+
+
+function levelUp(){
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    //random btn choose
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randColor}`)
+    btnFlash(randbtn);
+}
+
+function btnPress(){
+    let btn = this;
+    btnFlash(btn);
+    
+}
+
+let allBtns = document.querySelectorAll(".btn");
+
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
+}
