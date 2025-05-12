16af7f42f962f3d1674aa23534490ecd10a9c247
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-12 22:57:50 +0530

    init

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,3 +1,3 @@
-const someValue = require("./math");
+const someValue = require("./math"); // a built-in function external module that exixts in sparate files
 
 console.log(someValue(5,10));
--- NodeJs/math.js
+++ NodeJs/math.js
@@ -3,4 +3,4 @@ const mul = (a,b) => a*b;
 const sub = (a,b) => a-b;
 
 
-module.exports = sum;
+module.exports = sum; // a special objects 
