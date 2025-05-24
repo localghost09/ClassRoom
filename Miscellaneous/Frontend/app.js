5681ea6846d330e6fdf97d963cd4528474124881
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-24 23:45:15 +0530

    New Operator

--- Miscellaneous/Frontend/app.js
+++ Miscellaneous/Frontend/app.js
@@ -33,3 +33,19 @@ const st = {
 //     };
 //     return person;
 // }
+
+// New Operator
+// Constructors - doesn't return anything & start with capital
+
+function PersonMaker(name,age){
+    this.name = name;
+    this.age = age;
+}
+
+PersonMaker.prototype.talk = function (){
+    console.log(`hi , my name is ${this.name}`);
+    
+}
+
+let p1 = new PersonMaker("nikhil",25);
+let p2 = new PersonMaker("kannu", 23);
