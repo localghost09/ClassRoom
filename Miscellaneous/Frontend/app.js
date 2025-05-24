44fca1e7de4655360c96e140f6def489698c3fd6
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-24 23:22:01 +0530

    Factory Function

--- Miscellaneous/Frontend/app.js
+++ Miscellaneous/Frontend/app.js
@@ -8,13 +8,28 @@ const st = {
 //Prototype
 
 
-let arr1 = [1,2,3];
-let arr2 = [1,2,3];
+// let arr1 = [1,2,3];
+// let arr2 = [1,2,3];
 
-arr1.sayhello = () =>{
-    console.log("hello , i am arr");
+// arr1.sayhello = () =>{
+//     console.log("hello , i am arr");
     
-}
+// }
 
-arr2.sayhello = () =>{console.log("hello i am arr");
-}
+// arr2.sayhello = () =>{console.log("hello i am arr");
+// }/
+
+// Factory Function
+// A function that creates objects
+
+// function PersonMaker(name,age){
+//     const person = {
+//         name:name,
+//         age: age,
+//         talk(){
+//             console.log(`Hi , my name is ${this.name}`);
+            
+//         },
+//     };
+//     return person;
+// }
