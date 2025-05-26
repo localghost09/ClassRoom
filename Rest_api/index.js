4da89877bef6855d032c9ac3be1a08117a79d151
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-05-26 23:07:52 +0530

    Index Route

--- Rest_api/index.js
+++ Rest_api/index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
