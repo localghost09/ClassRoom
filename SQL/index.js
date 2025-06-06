98fe52b0890931055b95ea612cd571979ee5cf0c
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-06-06 23:55:37 +0530

    Insert INto user

--- SQL/index.js
+++ SQL/index.js
@@ -8,8 +8,14 @@ const connection = mysql.createConnection({
   password:'MERNSTACK',
 });
 
+//Inserting New Data
+let q = "INSERT INTO user (id, username, email, password) VALUES (? , ? , ?, ?)";
+let user = ["123", "123_newuser", "abcd@gamil.com", "abcde"]
+
+
+
 try{
-connection.query("SHOW TABLES", (err,result)=>{
+connection.query(q,user, (err,result)=>{
   if(err) throw err;
   console.log(result);
   
