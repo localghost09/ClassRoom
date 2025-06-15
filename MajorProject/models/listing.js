cfe47413dd686f9c7a03cd915f725f975add845e
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-06-15 21:20:47 +0530

    initialize database

--- MajorProject/models/listing.js
+++ MajorProject/models/listing.js
@@ -3,16 +3,15 @@ const Schema = mongoose.Schema;
 
 
 const listingSchema = new Schema({
-    titile : {
+    title : {
         type:String,
         required : true
     },
     description : String,
 
     image : {
-        type : String,
-        default : "https://images.unsplash.com/photo-1490730141103-6cac27aaab94?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGlmZXxlbnwwfHwwfHx8MA%3D%3D",
-        set : (v) => v === "" ? "https://images.unsplash.com/photo-1490730141103-6cac27aaab94?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGlmZXxlbnwwfHwwfHx8MA%3D%3D" : v
+        filename : String,
+        url : String,
     },
     price : Number,
     location : String,
