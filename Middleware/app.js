dfe4b6551174a4cfcebf532dcb84207b64ab04c8
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-07-22 22:59:40 +0530

    creating 1st middleware

--- Middleware/app.js
+++ Middleware/app.js
@@ -3,11 +3,21 @@ const app = express();
 
 //middleware -> response send
 
+app.use((req,resp,next)=>{
+    console.log("hi , i am middleware");
+    next();
+    
+});
+
 app.get("/",(req,resp)=>{
-    resp.send()
-})
+    resp.send("hey i am root"); 
+});
+
+app.get("/random", (req,resp)=>{
+    resp.send("this is random page");
+});
 
 app.listen(8080, ()=>{
     console.log("Server Listing to port 8080");
     
-})
+});
