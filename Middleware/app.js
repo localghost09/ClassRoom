3f302451b3aa1c81b04c738c7e81955088eb6b37
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-07-24 01:34:36 +0530

    Api token as a query string

--- Middleware/app.js
+++ Middleware/app.js
@@ -9,13 +9,24 @@ const app = express();
     
 // });
 
+app.use("/api",(req,resp,next)=>{
+    let {tokens} = req.query;
+    if(tokens === "giveaccess"){
+        next();
+    }
+    resp.send("ACCESS DENIED"); 
+});
+
+app.get("/api",(req,resp)=>{
+    resp.send("data");
+})
 
 // Logger -morgan
-app.use((req,resp,next)=>{
-    req.time = new Date(Date.now()).toString();
-    console.log(req.method, req.hostname,req.path,req.time);
-    next(); 
-})
+// app.use((req,resp,next)=>{
+//     req.time = new Date(Date.now()).toString();
+//     console.log(req.method, req.hostname,req.path,req.time);
+//     next(); 
+// })
 
 app.get("/",(req,resp)=>{
     resp.send("hey i am root"); 
