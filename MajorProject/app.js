e41954169bf9525514240a25bf2df6baa1e0616a
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-08-02 14:23:13 +0530

    init

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -39,7 +39,7 @@ const validateListing = (req,resp,next)=>{
     let {error} = listingSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
+        throw new ExpressError(400,errMsg );
     }else{
         next();
     }
