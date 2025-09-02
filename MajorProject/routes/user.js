450da1daeaa0a8e0ce297d27747f6e2bf1a0dfab
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2025-09-03 01:27:19 +0530

    login user

--- MajorProject/routes/user.js
+++ MajorProject/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsycn = require("../utils/wrapAsycn");
+const passport = require("passport");
 
 
 router.get("/signup", (req,resp)=>{
@@ -11,18 +12,26 @@ router.get("/signup", (req,resp)=>{
 router.post("/signup", wrapAsycn(async(req,resp)=>{
 
     try{
-    let {username,email,password} = req.body;
-    const newUser = new User ({email , username});
-    const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser);
-    req.flash("success", "Welcome to Wanderlust");
-    resp.redirect("/listings");
+        let {username,email,password} = req.body;
+        const newUser = new User ({email , username});
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.flash("success", "Welcome to Wanderlust");
+        resp.redirect("/listings");
     } catch(e){
         req.flash("error", e.message);
         resp.redirect("/signup");
     }
+    })
+);
+    
+router.get("/login", (req,resp)=>{
+    resp.render("users/login.ejs")
+})
 
-   
-}));
+router.post("/login",passport.authenticate("local",{failureRedirect : "/login", failureFlash : true,}), async(req,resp)=>{
+    req.flash("success", "Welcome back to wanderlust!");
+    resp.redirect("/listings");
+})
 
 module.exports = router;
